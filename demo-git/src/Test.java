6f6bdf8e0dc5e517ab2a934a663c4dafc446d3d1
sistemasiq <Argus@DESKTOP-RUUR7E8>
2018-04-06 11:28:38 -0500

    Se agrego metodo para el calculo del area de un triangulo

--- demo-git/src/Test.java
+++ demo-git/src/Test.java
@@ -4,5 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hola Git!");
 	}
+	
+	public static int getArea(int base, int altura){
+		int area = base * altura / 2;
+		return area;
+	}
 
 }
