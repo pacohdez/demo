3848aaea6f84adbc1d2a1ff494649430a34f3727
Usuario <Usuario@DESKTOP-5IA4HJQ>
2018-04-12 14:26:03 -0500

    Pintado del área del triángulo

--- demo-git/src/Test.java
+++ demo-git/src/Test.java
@@ -10,10 +10,12 @@ public class Test {
 		System.out.println("AUTOR: ANDREA");
 		System.out.println("Author: Jakqueline");
 		System.out.println("Andale Paco si se puede!!!");
+		
 	}
 	
 	public static int getArea(int base, int altura){
 		int area = base * altura / 2;
+		System.out.println("Area: "+area);
 		return area;
 	}
 
