fab6260a114a2b28aed44f810471d8b88787628d
Admin <Admin@DESKTOP-LNL6IU7.mshome.net>
2018-04-12 14:33:55 -0500

    Parametros del metodo getArea y modificacion del system con mi nombre

--- demo-git/src/Test.java
+++ demo-git/src/Test.java
@@ -4,10 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hola Git!");
 		
-		int area = getArea(6,10);
+		int area = getArea(5,24);
 		System.out.println("Area:" +area);
 		System.out.println("Author: Lilian");
-		System.out.println("AUTOR: ANDREA");
+		System.out.println("Author: Andrea Adriana (>.<)");
 		System.out.println("Author: Jakqueline");
 		System.out.println("Andale Paco si se puede!!!");
 	}
