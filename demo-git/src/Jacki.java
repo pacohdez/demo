ac323829b24299dc649c0d871aacb8c2b99dec74
Admin <Admin@DESKTOP-4C64H4S>
2018-04-12 14:31:21 -0500

    Se agrego clase Jacki con 2 metodos

---
+++ demo-git/src/Jacki.java
@@ -0,0 +1,16 @@
+
+public class Jacki {
+	public static void main(String args[]){
+		System.out.println("Suma: "+sumar(5,8));
+		System.out.println("Multiplicación: "+multiplicar(5,8));
+	}
+	
+	
+	public static int sumar(int numero1,int numero2){
+		return numero1+numero2;
+	}
+	
+	public static int multiplicar(int numero1,int numero2){
+		return numero1*numero2;		
+	}
+}
