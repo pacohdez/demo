0f753cbb42264fe0daae5357a440c8ec629f7f5c
Admin <Admin@DESKTOP-4C64H4S>
2018-04-14 08:49:48 -0500

    Se quitó método main

--- demo-git/src/Jacki.java
+++ demo-git/src/Jacki.java
@@ -1,11 +1,5 @@
 
 public class Jacki {
-	public static void main(String args[]){
-		System.out.println("Suma: "+sumar(5,8));
-		System.out.println("Multiplicación: "+multiplicar(5,8));
-	}
-	
-	
 	public static int sumar(int numero1,int numero2){
 		return numero1+numero2;
 	}
