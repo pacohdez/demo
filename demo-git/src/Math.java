a8946046393d0cf4a9329b228201a2d93e36764c
paco <admin@DESKTOP-2Q4LFIN>
2018-04-20 14:14:24 -0500

    Se modificó el nombre de los parametros del metodo sumar

--- demo-git/src/Math.java
+++ demo-git/src/Math.java
@@ -1,7 +1,7 @@
 
 public class Math {
-	public static int sumar(int numero1,int numero2){
-		return numero1+numero2;
+	public static int sumar(int num1,int num2){
+		return num1+num2;
 	}
 	
 	public static int multiplicar(int numero1,int numero2){
